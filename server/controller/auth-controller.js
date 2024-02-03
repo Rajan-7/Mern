d66ba3b0d8a9acb6ff3598a04a3321d70afb6dd5
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2024-02-03 21:44:25 +0545

    Feat:dummy data added to DB

--- server/controller/auth-controller.js
+++ server/controller/auth-controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user-model");
+
 const home = async (req, res) => {
   try {
     res.status(200).send("Controller Mern page");
@@ -8,8 +10,19 @@ const home = async (req, res) => {
 
 const register = async (req, res) => {
   try {
-    console.log(req.body);
-    res.status(200).send({message:req.body});
+   
+    const {username,email,phone,password} = req.body;
+
+    const userExist = await User.findOne({email});
+
+    if(userExist){
+      return res.status(400).json({msg:"Email already exist"});
+    }
+
+    const resData = await User.create({username,email,phone,password});
+
+    res.status(200).json({resData});
+    // console.log(resData);
   } catch (error) {
     res.status(400).send({ msg: "Page not found" });
   }
