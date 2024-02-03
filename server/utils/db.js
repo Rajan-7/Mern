44895cd5f0a8c87159573cf352d5fa846493d16f
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2024-02-03 17:52:43 +0545

    Feat:connected to database

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const URI ="mongodb://127.0.0.1:27017/";
+// const URI ="mongodb://127.0.0.1:27017/mern_admin";
+const URI =process.env.MONGO_URI;
 
 const connectDb = async()=>{
     try {
