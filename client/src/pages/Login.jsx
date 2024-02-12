a898adb9754947af46880fc12105ec27ea227327
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2024-02-12 21:38:50 +0545

    Feat:login form created

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,3 +1,78 @@
-export const Login = () =>{
-    return <h1>Login PAGE</h1>
-}
+import { useState } from "react";
+
+export const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+  return (
+    <>
+      <section>
+        <div className="section-login">
+          <div className="registration-container grid grid-two-cols">
+            <div className="registration-image">
+              <img
+                src="/image/register.png"
+                alt="Boy registring his information in website"
+                width="600"
+                height="600"
+              />
+            </div>
+            <div className="registration-form">
+              <h1 className="main-heading">Login form</h1>
+              <br />
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label htmlFor="email">email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Enter your email"
+                    id="email"
+                    required
+                    autoComplete="off"
+                    value={user.email}
+                    onChange={handleInput}
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="password">password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="password"
+                    id="password"
+                    required
+                    autoComplete="off"
+                    value={user.password}
+                    onChange={handleInput}
+                  />
+                </div>
+                <br />
+                <button type="submit" className="btn-submit">
+                  Login
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
