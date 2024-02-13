4f59d611c0550fc767785e7230a7623be823d48c
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2024-02-13 22:33:35 +0545

    Feat:Home and About page completed

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,77 @@
-export const About = () =>{
-    return <h1>About PAGE</h1>
-}
+export const About = () => {
+  return (
+    <>
+      {/* 1st section*/}
+      <main>
+        <section className="hero-section">
+          <div className="container grid grid-two-cols">
+            <div className="hero-content">
+              <p>Welcome,Jkovic Brady</p>
+              <h1>Why Choose Us?</h1>
+              <p>
+                Expertise: Our team consists of experienced IT professionals who
+                are passionate about staying up-to date with latest industry
+                trends.
+              </p>
+              <p>
+                Customization: We understand every business is unique.That's why
+                we create solutions that are that are tailored to your specific
+                needs & goals.
+              </p>
+              <p>
+                Cutomer-centric approach: We prioritize your satisfaction and
+                provide top-notch support ot address your IT concerns.
+              </p>
+              <p>
+                Affordability: We offer competitive pricing without compromising
+                on the quality of our services.
+              </p>
+              <p>
+                Expertise: Count on us to be there when you need us. We're
+                committed to ensuring your IT environment is reliable &
+                available 24/7.
+              </p>
+              <div className="btn btn-group">
+                <a href="/contact">
+                  <button className="btn">Contact Now</button>
+                </a>
+                <a href="/service">
+                  <button className="btn secondary-btn">Learn More</button>
+                </a>
+              </div>
+            </div>
+            <div className="hero-image">
+              <img
+                src="/image/about1.png"
+                alt="A girl showing content"
+                height="600"
+                width="500"
+              />
+            </div>
+          </div>
+        </section>
+      </main>
+      {/* 2nd section */}
+      <section className="section-analytics">
+        <div className="container grid grid-four-cols">
+          <div className="div1">
+            <h2>50+</h2>
+            <p>Registered companies</p>
+          </div>
+          <div className="div1">
+            <h2>10,000+</h2>
+            <p>Happy clients</p>
+          </div>
+          <div className="div1">
+            <h2>500+</h2>
+            <p>Well known developer</p>
+          </div>
+          <div className="div1">
+            <h2>24/7</h2>
+            <p>Services</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 export const Home = () => {
   return (
     <>
+      {/* 1st section */}
       <main>
         <section className="hero-section">
           <div className="container grid grid-two-cols">
@@ -32,28 +33,59 @@ export const Home = () => {
             </div>
           </div>
         </section>
-        {/* 2nd section */}
-        <section className="section-analytics">
-          <div className="container grid grid-four-cols">
-            <div className="div1">
-              <h2>50+</h2>
-              <p>Registered companies</p>
-            </div>
-            <div className="div1">
-              <h2>10,000+</h2>
-              <p>Happy clients</p>
-            </div>
-            <div className="div1">
-              <h2>500+</h2>
-              <p>Well known developer</p>
-            </div>
-            <div className="div1">
-              <h2>24/7</h2>
-              <p>Services</p>
+      </main>
+      {/* 2nd section */}
+      <section className="section-analytics">
+        <div className="container grid grid-four-cols">
+          <div className="div1">
+            <h2>50+</h2>
+            <p>Registered companies</p>
+          </div>
+          <div className="div1">
+            <h2>10,000+</h2>
+            <p>Happy clients</p>
+          </div>
+          <div className="div1">
+            <h2>500+</h2>
+            <p>Well known developer</p>
+          </div>
+          <div className="div1">
+            <h2>24/7</h2>
+            <p>Services</p>
+          </div>
+        </div>
+      </section>
+      {/* 3rd section */}
+      <section className="hero-section">
+        <div className="container grid grid-two-cols">
+          <div className="hero-image">
+            <img
+              src="/image/homedesign.png"
+              alt="A girl coding"
+              height="500"
+              width="500"
+            />
+          </div>
+          <div className="hero-content">
+            <p>We are here to help you</p>
+            <h1>Get Started Today</h1>
+            <p>
+              Ready to take the first step towards a more efficient and secure
+              IT infrastructure? Contact us today for a free consultation and
+              let's discuss how Thapa Technical can help your business thrive in
+              the digital age.
+            </p>
+            <div className="btn btn-group">
+              <a href="/contact">
+                <button className="btn">Contact Now</button>
+              </a>
+              <a href="/service">
+                <button className="btn secondary-btn">Learn More</button>
+              </a>
             </div>
           </div>
-        </section>
-      </main>
+        </div>
+      </section>
     </>
   );
 };
