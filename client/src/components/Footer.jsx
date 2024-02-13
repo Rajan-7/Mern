3e231e68cd23d078c50618dde54856b9072e453c
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2024-02-13 22:40:24 +0545

    Feat:component(Footer.jsx) added

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+export const Footer = ()=>{
+    return <>
+        <footer>
+            <div className="footer-section">
+             <p>&copy;RJNBEAST7</p>
+            </div>
+        </footer>
+    </>
+}
